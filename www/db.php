c49e081a2ca0d0e911e3d0fabce9dff1d63d6d8c
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-07-01 20:51:41 +0200

    commit

--- www/db.php
+++ www/db.php
@@ -1,5 +1,5 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-$con=mysqli_connect("localhost","root","","tuksbooks") or die("Could not connect to database");
+$con=mysqli_connect("192.168.88.240","root","","tuksbooks") or die("Could not connect to database");
 
 ?>
