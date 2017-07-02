1391a2ad0c5f9fa732fedef14d450b189c4516e5
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-07-02 16:55:52 +0200

    commit

---
+++ www/scripts/FindBooks.js
@@ -0,0 +1,30 @@
+﻿var url = "http://192.168.88.240/tuksbooks/json.php";
+var url1 = "https://learnwebcode.github.io/json-example/animals-1.json";
+var ourRequest = new XMLHttpRequest();
+ourRequest.open("GET", url1);
+ourRequest.onload = function () {
+
+    var ourData = JSON.parse(ourRequest.responseText);
+    document.getElementById("par").innerHTML = ourData[0].name
+    //console.log(ourData[0].name);
+};
+ourRequest.send();
+
+/*
+$(document).ready(function(){
+
+$.getJSON(url,function(result){
+//console.log(result);
+$.each(result,function(i,field){
+var id=field.id;
+var title=field.Title;
+var faculty=field.Faculty;
+$("p").text(id + " " + title + " " + faculty);
+
+});
+
+});
+
+
+
+});*/
