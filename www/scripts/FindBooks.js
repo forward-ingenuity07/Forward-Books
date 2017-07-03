28b2246212e73eea60cf9b0006b7c07ec1759ddb
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-07-03 15:09:16 +0200

    commit

--- www/scripts/FindBooks.js
+++ www/scripts/FindBooks.js
@@ -1,16 +1,21 @@
-﻿var url = "http://192.168.88.240/tuksbooks/json.php";
-var url1 = "https://learnwebcode.github.io/json-example/animals-1.json";
-var ourRequest = new XMLHttpRequest();
-ourRequest.open("GET", url1);
-ourRequest.onload = function () {
+﻿(function () {
 
-    var ourData = JSON.parse(ourRequest.responseText);
-    document.getElementById("par").innerHTML = ourData[0].name
-    //console.log(ourData[0].name);
-};
-ourRequest.send();
+    var url = "http://192.168.88.240:80/tuksbooks/json.php";
+    var url1 = "http://gobooks.000webhostapp.com/json.php";
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open("GET", url1);
+    var type = 0;
+    ourRequest.onload = function () {
+        type = 1;
+        var ourData = JSON.parse(ourRequest.responseText);
+        document.getElementById("par").innerHTML = ourData[0].title;
+    };
+    ourRequest.send();
+    if (type != 1) {
+        document.getElementById("par").innerHTML = "Connecting...";
 
-/*
+    }
+    /*
 $(document).ready(function(){
 
 $.getJSON(url,function(result){
@@ -28,3 +33,5 @@ $("p").text(id + " " + title + " " + faculty);
 
 
 });*/
+
+});
