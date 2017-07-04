215ab36ff6004fd981704274667e51757c8d3866
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-07-04 22:48:07 +0200

    commit

--- www/scripts/jayq.js
+++ www/scripts/jayq.js
@@ -1,17 +1,17 @@
 ﻿$(document).ready(function () {
-    start = 0;
-    $(document).scroll(function () {
+    start = 0;          //Value at top of scroll bar
+    $(document).scroll(function () {        //Upon scroll event
         
-        if($(window).scrollTop().valueOf()==start)
+        if($(window).scrollTop().valueOf()==start)  //Remember the .valueOf(), otherwise it will only return an object
         {
-            $(".navbar-default").css('background-color', '#f8f8f8');
+            $(".navbar-default").css('background-color', '#f8f8f8');        //Change the colour of the navbar-default class
 
 
         }
         else {
             
             
-            $(".navbar-default").css('background-color', 'transparent');
+            $(".navbar-default").css('background-color', 'transparent');    //Change colour
             
         }
         
