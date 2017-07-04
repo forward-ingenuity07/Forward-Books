cc5860dc7c02b96dc9909116aaeaba508814a9df
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-07-04 16:23:42 +0200

    commit

---
+++ www/scripts/jayq.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    start = 0;
+    $(document).scroll(function () {
+        
+        if($(window).scrollTop().valueOf()==start)
+        {
+            $(".navbar-default").css('background-color', '#222');
+
+
+        }
+        else {
+            
+            
+            $(".navbar-default").css('background-color', 'transparent');
+            
+        }
+        
+
+
+
+    });
+});
